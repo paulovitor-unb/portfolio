37a7caf26b6de886e7f3c07f8917dc70cb945b90
PauloVDosSantos <paulovitor.unb@gmail.com>
2023-02-24 18:56:43 -0300

    Version 1.0.0
    First version of Website

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 setupEventHandling();
 
 function setupEventHandling() {
+    changeActiveButton();
     changeBackgroundColorHue();
     changeArrowDirection();
+}
+
+function changeActiveButton() {
+    const buttons = document.querySelectorAll("nav button");
+
+    buttons?.forEach(button => {
+        button.addEventListener("click", function () {
+            if (button.classList.contains("active")) {
+                return;
+            }
+
+            button.parentElement
+                .querySelector("button.active")
+                .classList.remove("active");
+
+            button.classList.add("active");
+        });
+    });
 }
 
 function changeBackgroundColorHue() {
     const root = document.querySelector(":root");
     const range = document.querySelector("input");
 
-    range?.addEventListener("change", () => {
+    range?.addEventListener("change", function () {
         root.style.setProperty("--color-hue", range.value);
     });
 }
@@ -18,7 +37,7 @@ function changeArrowDirection() {
     const headers = document.querySelectorAll("main section header");
 
     headers?.forEach(header => {
-        header.addEventListener("click", () => {
+        header.addEventListener("click", function () {
             header.querySelector("svg").classList.toggle("rotate");
         });
     });
