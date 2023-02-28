c840c684d48aa8b51723de4ff80ad49016f14d40
Paulo Vitor <paulovitor.unb@gmail.com>
2023-02-28 13:54:08 -0300

    Create main components for the page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
+export default App;
