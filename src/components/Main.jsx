cdfebbe4879b05eaa7cbbd09cacfa7edf1feab2d
Paulo Vitor <paulovitor.unb@gmail.com>
2023-02-28 13:54:31 -0300

    Component for main content for the page

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+    return (
+        <main>
+            <section id="home"></section>
+            <section id="about"></section>
+            <section id="projects"></section>
+        </main>
+    );
+}
+export default Main;
