534ea66f4e89d7e558dcf48655f8447fe7d91643
Paulo Vitor <paulovitor.unb@gmail.com>
2023-03-02 09:55:46 -0300

    Create context for monitoring screen size

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import { createContext, useState, useEffect } from "react";
+
 import Header from "./components/Header/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer/Footer";
 
+export const DeviceContext = createContext(null);
+
 function App() {
+    const [isPhone, setIsPhone] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 767px)");
+        setIsPhone(mediaQuery.matches);
+
+        mediaQuery.addEventListener("change", () => {
+            setIsPhone(mediaQuery.matches);
+        });
+    }, []);
+
     return (
-        <>
+        <DeviceContext.Provider value={isPhone}>
             <Header />
             <Main />
             <Footer />
-        </>
+        </DeviceContext.Provider>
     );
 }
 export default App;
